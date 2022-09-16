30170487133ab337556f5be887e66d4ce35dabaa
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2022-09-16 18:19:41 -0400

    factura

--- lib/src/factura.dart
+++ lib/src/factura.dart
@@ -12,26 +12,74 @@ class _FacturaState extends State<Factura> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Factura')),
-        body: Table(
-          children: [
-            const TableRow(
-              children: [
-                Text('Hamburguesa'),
-                Text('Cantidad'),
-                Text('Total')
-              ]
-            ),
-            for (int j = 0; j < cantidad.length; j++)
-              // for(var a in cantidad)
-              TableRow(children: [
-                Text(hamburguesas[j]),
-                Text('${cantidad[j]}'),
-                Text('${cantidad[j] * precios[j]}')
-              ])
-            // TableRow()
-          ],
-        ));
+      appBar: AppBar(title: Text('Factura')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Table(
+            // defaultColumnWidth: FixedColumnWidth(130),
+            border: TableBorder.all(),
+            columnWidths: const {
+              0: FractionColumnWidth(0.45),
+              1: FractionColumnWidth(0.3),
+              2: FractionColumnWidth(0.2),
+            },
+            children: [
+              const TableRow(children: [
+                Text(
+                  textAlign: TextAlign.center,
+                  'Hamburguesa',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  textAlign: TextAlign.center,
+                  'Cantidad',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Total',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                )
+              ]),
+              for (int j = 0; j < cantidad.length; j++)
+                // for(var a in cantidad)
+                TableRow(children: [
+                  Text(
+                    hamburguesas[j],
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  Text(
+                    textAlign: TextAlign.center,
+                    '${cantidad[j]}',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  Text(
+                    textAlign: TextAlign.center,
+                    '${cantidad[j] * precios[j]}',
+                    style: TextStyle(fontSize: 25),
+                  )
+                ])
+              // TableRow()
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                'TOTAL A PAGAR',
+                style: TextStyle(fontSize: 25),
+              ),
+              Text(
+                '$total Bs',
+                style: TextStyle(fontSize: 25),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 
   void contar() {}
